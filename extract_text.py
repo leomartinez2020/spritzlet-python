93a8dc3945f625efe637a4421cec6b570c2a3316
leomartinez2019 <lowenhard@yahoo.com>
2019-10-11 06:49:57 -0500

    improve screen

--- extract_text.py
+++ extract_text.py
@@ -1,4 +1,6 @@
 import time
+import re
+import random
 import pickle
 import requests
 from bs4 import BeautifulSoup
@@ -16,26 +18,52 @@ def crawl_text(text):
     return leeme
 
 def test1():
-    #url = "https://hackernoon.com/functional-programming-what-language-should-you-be-talking-313dd8bc379b"
-    #url = "https://www.techrepublic.com/article/how-to-get-a-developer-job-the-best-programming-languages-to-learn/"
-    #url = "https://www.zdnet.com/article/linus-torvalds-isnt-worried-about-microsoft-taking-over-linux/"
-    url = "https://www.forbes.com/sites/googlecloud/2019/10/02/want-to-become-an-ai-first-company-hire-these-people/#601606705c72"
-    text = extract_from_url(url)
+    url1 = "https://hackernoon.com/functional-programming-what-language-should-you-be-talking-313dd8bc379b"
+    url2 = "https://www.techrepublic.com/article/how-to-get-a-developer-job-the-best-programming-languages-to-learn/"
+    url3 = "https://www.zdnet.com/article/linus-torvalds-isnt-worried-about-microsoft-taking-over-linux/"
+    url4 = "https://www.forbes.com/sites/googlecloud/2019/10/02/want-to-become-an-ai-first-company-hire-these-people/#601606705c72"
+    url5 = "https://www.geekwire.com/2019/amazon-launches-aws-iq-marketplace-third-party-cloud-experts-demand-projects/"
+    text = extract_from_url(url5)
     #print(crawl_text(text))
     return crawl_text(text)
 
-def pickle_data():
-    data = test1()
-    with open('datos', 'wb') as foo:
-        pickle.dump(data, foo, pickle.HIGHEST_PROTOCOL)
+url1 = "https://hackernoon.com/functional-programming-what-language-should-you-be-talking-313dd8bc379b"
+url2 = "https://www.techrepublic.com/article/how-to-get-a-developer-job-the-best-programming-languages-to-learn/"
+url3 = "https://www.zdnet.com/article/linus-torvalds-isnt-worried-about-microsoft-taking-over-linux/"
+url4 = "https://www.forbes.com/sites/googlecloud/2019/10/02/want-to-become-an-ai-first-company-hire-these-people/#601606705c72"
+url5 = "https://www.geekwire.com/2019/amazon-launches-aws-iq-marketplace-third-party-cloud-experts-demand-projects/"
+url6 = "https://www.businessinsider.com/10-key-skills-to-succeed-as-a-cloud-architect-2019-9"
 
-def extract_pickle():
-    with open('datos', 'rb') as foo:
+url_list = [url1, url2, url3, url4, url5, url6]
+
+def test2(url):
+    text = extract_from_url(url)
+    return crawl_text(text)
+
+def pickle_text(textfile):
+    with open(textfile, 'r') as bar:
+        content = bar.read()
+    lista = content.split()
+    filename = random.choice(lista) + random.choice(lista)
+    with open(filename, 'wb') as foo:
+        pickle.dump(lista, foo, pickle.HIGHEST_PROTOCOL)
+    print(f"file {filename} created...")
+
+def pickle_data(url):
+    name = re.search("www\.(\w+)\.com", url)
+    filename = name.group()
+    data = test2(url)
+    with open(filename, 'wb') as foo:
+        pickle.dump(data, foo, pickle.HIGHEST_PROTOCOL)
+    print(f"file {filename} created...")
+
+def extract_pickle(filename):
+    with open(filename, 'rb') as foo:
         datos = pickle.load(foo)
     return datos
 
-def read_text():
-    with open('datos', 'rb') as foo:
+def read_text(filename):
+    with open(filename, 'rb') as foo:
         d = pickle.load(foo)
     length = len(d)
     print(length)
--- teka2.py
+++ teka2.py
@@ -1,8 +1,14 @@
+import sys
 import tkinter as tk
 from extract_text import extract_pickle
 
-counter = 0 
-d = extract_pickle()
+counter = 0
+def get_data(filename):
+    return extract_pickle(filename)
+
+#d = get_data("www.geekwire.com")
+if len(sys.argv) > 1:
+    d = get_data(sys.argv[1])
 
 def counter_msg(msg):
   def count():
@@ -13,16 +19,19 @@ def counter_msg(msg):
     pad = ''.join([" "]*4)
     word = pad + word + pad
     counter += 1
-    msg.config(text="{:-^30}".format(word), fg="white", bg="grey", font=('arial', 64, 'bold'), width=900, aspect=300, justify=tk.RIGHT)
-    msg.after(120, count)
+    msg.config(text="{:-^30}".format(word), fg="black", bg="grey", font=('arial', 64, 'bold'), width=900, aspect=300, justify=tk.RIGHT)
+    msg.after(220, count)
   count()
  
  
 root = tk.Tk()
+root.geometry("1300x700")
+root.configure(background="grey")
 root.title("Counting Seconds")
+
 msg = tk.Message(root)
-msg.pack()
+msg.pack(pady=300)
 counter_msg(msg)
-button = tk.Button(root, text='Stop', width=80, command=root.destroy)
-button.pack()
+#button = tk.Button(root, text='Stop', width=80, command=root.destroy)
+#button.pack()
 root.mainloop()
